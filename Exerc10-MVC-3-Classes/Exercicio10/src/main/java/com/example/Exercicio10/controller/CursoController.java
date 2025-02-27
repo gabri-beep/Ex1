8fe3f1a2703f9a1c0819eb559cfe1aee2b1d93bf
gabri-beep <biel790832157@gmail.com>
2025-02-27 02:44:42 -0300

    Exe10-Gabriel_Rodrigues

---
+++ Exerc10-MVC-3-Classes/Exercicio10/src/main/java/com/example/Exercicio10/controller/CursoController.java
@@ -0,0 +1,45 @@
+package com.example.Exercicio10.controller;
+
+import com.example.Exercicio10.banco.CursoBd;
+import com.example.Exercicio10.model.Curso;
+
+import java.util.List;
+
+public class CursoController {
+    CursoBd repository = new CursoBd();
+
+    public List<Curso> getAll(){
+        return repository.findAll();
+    }
+
+    public Curso getByIdCurso(Long idCurso) {
+        return repository.getByIdCurso(idCurso);
+    }
+    public Curso getByNumeroSala(Integer numeroSala){
+        return repository.getByNumeroSala(numeroSala);
+    }
+
+    public Curso getByNomeProfessor(String nomeProfessor){
+        return repository.getByNomeProfessor(nomeProfessor);
+    }
+
+    public boolean insertBanco(Curso curso){
+        return repository.insert(curso);
+    }
+
+    public  Curso update(Long idCurso, Curso curso ){
+        boolean result = repository.update(idCurso, curso);
+
+        if(result){
+            return curso;
+        }
+        return null;
+    }
+
+    public boolean delete(Long idCurso){
+       return repository.delete(idCurso);
+
+
+    }
+
+}
---
+++ Exerc10-MVC-3-Classes/Exercicio10/src/main/java/com/example/Exercicio10/model/Professor.java
@@ -0,0 +1,48 @@
+package com.example.Exercicio10.model;
+
+public class Professor {
+    private Long idProfessor;
+    private String nomeProfessor;
+    private String cpf;
+
+    private double salario;
+
+    public Professor(Long idProfessor, String nomeProfessor, String cpf, double salario) {
+        this.idProfessor = idProfessor;
+        this.nomeProfessor = nomeProfessor;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public Long getIdProfessor() {
+        return idProfessor;
+    }
+
+    public void setIdProfessor(Long idProfessor) {
+        this.idProfessor = idProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public void setNomeProfessor(String nomeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
